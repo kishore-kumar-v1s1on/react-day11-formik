202192c6a3bbfdbed3304144c3942bd3f28e5ec4
Gopi <GopiR5@yahoo.com>
2024-06-18 09:33:07 +0530

    added

---
+++ src/components/AddBook.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Form from "./Form";
+import { API } from "../api/api";
+
+const AddBook = () => {
+  // eslint-disable-next-line 
+  const [bookDetails, setBooktDetails] = useState({
+    name: "",
+    image: "",
+    author:"",
+    language:"",
+    publish:"",
+    
+    
+  });
+  
+ const navigate = useNavigate();
+  const addNewUser = async (newUser) => {
+
+    try {
+      const response = await fetch(
+        `${API}`,
+        {
+          method: "POST",
+          body: JSON.stringify(newUser),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      //setUser([...user, data])
+
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <Form type={"Add"} bookDetails={bookDetails} onSubmit={addNewUser} />
+    </div>
+  );
+};
+
+export default AddBook;
